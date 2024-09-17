0702d3aec24a8b23a13b17a0c1f9b3dae318e7d6
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-09-17 16:21:33 +0530

    Delete Code/04 Operators directory

--- Code/04 Operators/Logical.java
+++
@@ -1,7 +0,0 @@
-public class Logical {
-    public static void main(String[] args) {
-        // && --> AND
-        // || --> OR
-        // ! --> NOT
-    }
-}
--- Code/04 Operators/Unary.java
+++
@@ -1,7 +0,0 @@
-public class Unary {
-    public static void main(String[] args) {
-        int a = 10;
-        int b=a++;
-        System.out.println(b);
-    }    
-}
