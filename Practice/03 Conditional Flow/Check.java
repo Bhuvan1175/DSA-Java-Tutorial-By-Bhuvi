d287f70f8c8632076283f521cb441a8da3d665db
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-09-17 16:32:45 +0530

    Create Check.java

---
+++ Practice/03 Conditional Flow/Check.java
@@ -0,0 +1,10 @@
+// Question 4 : What will be the value of x & y in the following program:
+public class Check {
+    public static void main(String args[]) {
+        int a = 63, b = 36;
+        boolean x = (a < b ) ? true : false;
+        int y= (a > b ) ? a : b;
+        System.out.println(x);
+        System.out.println(y);
+        }
+}
