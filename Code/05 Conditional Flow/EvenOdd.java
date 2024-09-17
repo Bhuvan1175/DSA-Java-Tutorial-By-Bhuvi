1dae03d50f3f0d9f0a622a382c0b799acae069ac
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-09-17 16:22:16 +0530

    Create EvenOdd.java

---
+++ Code/05 Conditional Flow/EvenOdd.java
@@ -0,0 +1,10 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 10;
+        if(num%2==0){
+            System.out.println("Number is Even");
+        }else{
+            System.out.println("Number is Odd");
+        }
+    }
+}
