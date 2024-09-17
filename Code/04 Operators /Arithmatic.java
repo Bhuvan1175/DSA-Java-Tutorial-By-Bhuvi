9518b53d18b0b40ab1f4ca0f404a16010fc34dde
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-09-17 16:08:59 +0530

    Create Arithmatic.java

---
+++ Code/04 Operators /Arithmatic.java
@@ -0,0 +1,11 @@
+public class Arithmatic {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println("Addition : "+ (num1+num2));
+        System.out.println("Substraction : "+ (num1-num2));
+        System.out.println("Multiplication : "+ (num1*num2));
+        System.out.println("Division : "+ (num1/num2));
+        System.out.println("Modulo : "+ (num1%num2));
+    }
+}
