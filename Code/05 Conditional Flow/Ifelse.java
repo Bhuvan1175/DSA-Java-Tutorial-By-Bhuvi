eb7a075efc39425bff8e46bbeafbba1689a73938
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-09-17 16:22:44 +0530

    Add files via upload

---
+++ Code/05 Conditional Flow/Ifelse.java
@@ -0,0 +1,10 @@
+public class Ifelse {
+    public static void main(String[] args) {
+        int age=17;
+        if(age>=18){
+            System.out.println("You are eligible for voting");
+        }else{
+            System.out.println("You are not eligible for voting");
+        }
+    }
+}
