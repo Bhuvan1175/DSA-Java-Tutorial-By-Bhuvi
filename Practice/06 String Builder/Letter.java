383ba5d25999738896d1f71ef7f71fadfa6306ff
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-09-28 12:00:11 +0530

    Create Letter.java

---
+++ Practice/06 String Builder/Letter.java
@@ -0,0 +1,7 @@
+
+public class Letter {
+    public static void main(String[] args) {
+        String Letter="Dear <|name|>, Thanks a lot";
+        System.out.println(Letter.replace("<|name|>","Virat"));
+    }    
+}
